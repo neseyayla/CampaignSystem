918ffad3fc6fafa41951c640554c3f137ccd9b3f
neseyayla <n.yayla438@gmail.com>
2026-08-11 10:41:33 +0300

    chore: Swagger UI configuration

--- CampaignSystem/Program.cs
+++ CampaignSystem/Program.cs
@@ -12,6 +12,11 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/openapi/v1.json", "CampaignSystem API v1");
+    });
 }
 
 app.UseHttpsRedirection();
